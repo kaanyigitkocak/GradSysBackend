1207df1ece714ea8a27366e2b51997c69334ef63
Yiğit Kaaan Koçak <kaanyigitkocak@gmail.com>
2025-05-23 21:33:56 +0300

    a

--- src/gradProject/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ src/gradProject/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+620c608d634fe0026221b136d5fad6fe6e8facdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8776c15753bf9bbaa83028864751c9a86a8edda8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
